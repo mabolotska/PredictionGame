7400ac226f61e5145645e0c92babad37f90b5310
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2024-02-19 12:07:31 +0100

    added questionVC

---
+++ PredictionGame/QuestionVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PredictionGame
+//
+//  Created by Maryna Bolotska on 19/02/24.
+//
+
+import UIKit
+
+class QuestionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
